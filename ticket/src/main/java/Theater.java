0d36bc405c5b757eab45b581ca88078570cc62de
seungjo <f1v3@kakao.com>
2025-06-11 14:43:31 +0900

    feat: 극장 클래스 및 입장 메서드 구현

---
+++ ticket/src/main/java/Theater.java
@@ -0,0 +1,30 @@
+/**
+ * 극장을 구현하는 클래스
+ *
+ * @author Seungjo, Jeong
+ */
+public class Theater {
+
+    private TicketSeller ticketSeller;
+
+    public Theater(TicketSeller ticketSeller) {
+        this.ticketSeller = ticketSeller;
+    }
+
+    /**
+     * 극장에 입장하는 메서드
+     *
+     * @param audience 관람객 객체
+     */
+    public void enter(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+    }
+}
